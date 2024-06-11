b7ed0e2ae3b400eab32fc50702321f52aa45f2c5
Omar Malik <omar-malik22@hotmail.com>
2024-06-11 22:27:15 +0100

    dev!

---
+++ javascript/script.js
@@ -0,0 +1,17 @@
+// setTimeout(5000)
+// let inputNum = prompt("give us a number")
+
+// let number = Math.floor(Math.random() * inputNum) + 1
+// let guess = prompt("guess the random number!")
+// let count = 0
+// while(guess!==number){
+//     if(guess<number){
+//         count+=1
+//         guess = prompt("too low")
+//     }else if(guess>number){
+//         guess = prompt("too high")
+//     }else if(guess === "q"){
+//         break
+//     }
+// }
+// alert(`nice!, you guessed ${count} times`)
